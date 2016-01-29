823802bb04e0efd902a25932db5a39b946e65499
Michael L. Brock <michael.brock@gmail.com>
2016-01-29 10:52:29 -0500

    Add support for simple multi-target builds
    Copied from the MlbDev repo (mutatis mutandis).

---
+++ SimpleBuildDriverLocalList.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+# #############################################################################
+# #############################################################################
+# Multiple Architecture Source Code Production System (MASCaPS) Version 3
+# 	Simple Build Driver Local Target Directory List for MlbDevOld
+# #############################################################################
+#
+# File Name       : SimpleBuildDriverLocalList.sh
+#
+# File Description: Simple build driver local target list for MlbDevOld.
+#
+# Revision History: 2016-01-29 --- Creation.
+#                       Michael L. Brock
+#
+#       Copyright Michael L. Brock 2016 - 2016.
+#
+#       Distributed under the Boost Software License, Version 1.0.
+#       (See accompanying file LICENSE_1_0.txt or copy at
+#       http://www.boost.org/LICENSE_1_0.txt)
+#
+# #############################################################################
+
+# #############################################################################
+# ##### This is the complete list, which I'm not building at present.
+MY_DIR_LIST="\
+strfuncs/source \
+sdtif/source \
+genfuncs/source \
+datfuncs/source \
+dcr/source \
+dlls/source \
+dta/source \
+eecfuncs/source \
+kmlfuncs/source \
+mddl/source \
+mfile/source \
+mfiledos/source \
+mlog/source \
+MLogO/source \
+mmap/source \
+mmi/source \
+mregion/source \
+mtif/source \
+MtifO/source \
+npsl/source \
+NpslO/source \
+pcff/source \
+siidr/source \
+spffuncs/source \
+tfread/source \
+dbcfuncs/source \
+dbcnull/source \
+dbcsyb/source \
+dbcctlib/source \
+dbcdblib/source \
+dbcmssql/source \
+rvutil/source \
+MlbBins/source \
+adf/source\
+"
+# #############################################################################
+
+# #############################################################################
+# ##### This is the redacted list.
+MY_DIR_LIST="\
+strfuncs/source \
+sdtif/source \
+genfuncs/source \
+datfuncs/source \
+dcr/source \
+dlls/source \
+dta/source \
+eecfuncs/source \
+kmlfuncs/source \
+mddl/source \
+mfile/source \
+mfiledos/source \
+mlog/source \
+MLogO/source \
+mmap/source \
+mmi/source \
+mregion/source \
+mtif/source \
+MtifO/source \
+npsl/source \
+NpslO/source \
+pcff/source \
+siidr/source \
+spffuncs/source \
+tfread/source \
+dbcfuncs/source \
+dbcnull/source \
+dbcsyb/source \
+dbcctlib/source \
+dbcdblib/source \
+dbcmssql/source \
+rvutil/source \
+MlbBins/source \
+adf/source\
+"
+# #############################################################################
+
